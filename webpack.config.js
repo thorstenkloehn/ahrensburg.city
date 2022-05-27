5c4682f8e5310dad365f2dfe2ef1a1f4fba2dd92
Thorsten Klöhn <thorstenkloehn@gmail.com>
2022-05-27 18:38:51 +0200

    Anfang

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
                 use: {
                     loader: "file-loader",
                     options: {
-                        name: '[name].[ext]',
+                        name: 'images/[name].[ext]',
                     },
 
                 },
