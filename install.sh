943acc6989f02cc8ade0505c4801694650b68d0c
Thorsten Klöhn <thorstenkloehn@gmail.com>
2022-06-30 23:47:34 +0200

    Starten

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 
 sudo cp -u start.conf /etc/nginx/conf.d/start.conf
   sudo cp -u mediawiki.conf /etc/nginx/conf.d/mediawiki.conf
+  sudo cp -u wordpress.conf /etc/nginx/conf.d/wordpress.conf
 #  sudo cp -u englisch.conf /etc/nginx/conf.d/englisch.conf
 #  sudo cp -u lernen.conf /etc/nginx/conf.d/lernen.conf
 
