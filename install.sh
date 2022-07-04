2b900b8c7c08ed891c79d76f90009156d24c4db5
Thorsten Klöhn <thorstenkloehn@gmail.com>
2022-07-04 10:41:20 +0200

    Starten

--- install.sh
+++ install.sh
@@ -5,4 +5,5 @@ sudo cp -u start.conf /etc/nginx/conf.d/start.conf
   sudo cp -u php.conf /etc/nginx/conf.d/php.conf
   sudo cp -u typo3.conf /etc/nginx/conf.d/typo3.conf
   sudo cp -u wordpress.conf /etc/nginx/conf.d/wordpress.conf
+  sudo cp -u joomla.conf /etc/nginx/conf.d/joomla.conf
 
