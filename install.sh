4e121c626a5e32c0323b8e3fc5c54a1745ed07db
Thorsten Klöhn <thorstenkloehn@gmail.com>
2022-07-07 00:56:28 +0200

    Starten

--- install.sh
+++ install.sh
@@ -2,9 +2,5 @@
 sudo cp -u start.conf /etc/nginx/conf.d/start.conf
   sudo cp -u mediawiki.conf /etc/nginx/conf.d/mediawiki.conf
   sudo cp -u lernen.conf /etc/nginx/conf.d/lernen.conf
-  sudo cp -u php.conf /etc/nginx/conf.d/php.conf
-  sudo cp -u typo3.conf /etc/nginx/conf.d/typo3.conf
-  sudo cp -u wordpress.conf /etc/nginx/conf.d/wordpress.conf
-  sudo cp -u joomla.conf /etc/nginx/conf.d/joomla.conf
-  sudo cp -u drupal.conf /etc/nginx/conf.d/drupal.conf
+
 
