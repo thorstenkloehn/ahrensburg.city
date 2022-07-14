120f0026daf99c3a2f26f1e1d90895f39a34352c
Thorsten Klöhn <thorstenkloehn@gmail.com>
2022-07-15 01:02:54 +0200

    Start

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 sudo cp -u start.conf /etc/nginx/conf.d/start.conf
   sudo cp -u mediawiki.conf /etc/nginx/conf.d/mediawiki.conf
   sudo cp -u lernen.conf /etc/nginx/conf.d/lernen.conf
+  sudo cp -u nomination.conf /etc/nginx/conf.d/nomination.conf
   sudo cp -u photon.service /etc/systemd/system/photon.service
   sudo  systemctl enable photon.service
   sudo  systemctl start photon.service
