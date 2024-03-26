d6d4b68157e0c88c59909ec5c2a892c41f9db274
Thorsten Klöhn <thorstenkloehn@gmail.com>
2024-03-26 10:41:59 +0100

    start

--- components/Navigation.js
+++ components/Navigation.js
@@ -3,13 +3,27 @@ import Link from "next/link";
 
 export default function Navigation() {
     return (
-        <div>
-
-            <Link href="/">Startseite</Link> - 
-            <Link href="/ahrensburgkarte">Karte</Link> -
-            <Link href="/shop">Läden</Link> -
-            <Link href="/Impressum">Impressum</Link> - 
-            <Link href="/Datenschutz">Datenschutz</Link> 
+        <div className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link href="/" className="navbar-brand">Startseite</Link>
+            <div className="collapse navbar-collapse">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item">
+                        <Link href="/ahrensburgkarte" className="nav-link">Karte</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link href="/shop" className="nav-link">Läden</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link href="/Impressum" className="nav-link">Impressum</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link href="/Datenschutz" className="nav-link">Datenschutz</Link>
+                    </li>
+                    <li className="nav-item">
+                        <a href="https://thorstenkloehn.github.io/ahrensburg.city/" className="nav-link" target="_blank" rel="noopener noreferrer">Dokument</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     );
 }
