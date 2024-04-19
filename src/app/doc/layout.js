201ca7c28d016f69a533c796bde50fcee44b8dbf
Thorsten Klöhn <thorstenkloehn@gmail.com>
2024-04-19 11:49:25 +0200

    start

--- src/app/doc/layout.js
+++ src/app/doc/layout.js
@@ -11,7 +11,6 @@ export default function DocsLayout({ children }) {
               <li><Link href="/doc/Programmiersprachen">Programmiersprachen</Link></li>
               <li><Link href="/doc/Server_Mieten">Server Mieten</Link></li>
               <li><Link href="/doc/Analysetool">Analysetool</Link></li>
-              <li><Link href="/doc/SSGs">SSGs</Link></li>
               <li><Link href="/doc/Webentwicklung">Webentwicklung</Link></li>
               <li><Link href="/doc/Django">Django</Link></li>
               <li><Link href="/doc/PHP_kein">PHP Kein</Link></li>
