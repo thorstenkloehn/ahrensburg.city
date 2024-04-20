418fb020e7e96d8e5621b30ed73dd42d51672970
Thorsten Klöhn <thorstenkloehn@gmail.com>
2024-04-21 01:14:29 +0200

    start

--- next.config.js
+++ next.config.js
@@ -3,16 +3,17 @@ const withMDX = require('@next/mdx')()
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Configure `pageExtensions` to include MDX files
-  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
-  basePath: '/ahtrensburg.city',
-  assetPrefix: '/ahrensburg.city/',
-  output: 'export',
+ pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+ basePath: '/ahrensburg.city',
+ assetPrefix: '/ahrensburg.city/',
+
+ // output: 'export',
  
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  trailingSlash: true,
+   trailingSlash: true,
  
   // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
- skipTrailingSlashRedirect: true,
+   skipTrailingSlashRedirect: true,
  
   // Optional: Change the output directory `out` -> `dist`
  distDir: 'out',
