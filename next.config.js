2343a79db5777756d3f905decfe9882b2b110182
Thorsten Klöhn <thorstenkloehn@gmail.com>
2024-04-21 00:01:54 +0200

    start

--- next.config.js
+++ next.config.js
@@ -4,8 +4,19 @@ const withMDX = require('@next/mdx')()
 const nextConfig = {
   // Configure `pageExtensions` to include MDX files
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  basePath: '/ahtrensburg.city',
+  assetPrefix: '/ahrensburg.city/',
+  output: 'export',
  
-  }
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+ skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+ distDir: 'out',
+}
  
 
  
--- src/app/doc/layout.js
+++ src/app/doc/layout.js
@@ -8,13 +8,10 @@ export default function DocsLayout({ children }) {
             <ul className="flex flex-col space-y-1">
               <li><Link href="/doc">Server</Link></li>
               <li><Link href="/doc/IDE">IDE</Link></li>
-              <li><Link href="/doc/Programmiersprachen">Programmiersprachen</Link></li>
               <li><Link href="/doc/Server_Mieten">Server Mieten</Link></li>
               <li><Link href="/doc/Analysetool">Analysetool</Link></li>
               <li><Link href="/doc/Webentwicklung">Webentwicklung</Link></li>
-              <li><Link href="/doc/Django">Django</Link></li>
-              <li><Link href="/doc/PHP_kein">PHP Kein</Link></li>
-            </ul>
+              </ul>
           </div>
           <div className="flex-grow pl-7 w-full">{children}</div>
         </div>
