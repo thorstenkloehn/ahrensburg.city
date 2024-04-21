9a69bbed7032951ba225a4e141f82452056dd117
Thorsten Klöhn <thorstenkloehn@gmail.com>
2024-04-21 03:21:37 +0200

    start

--- src/app/doc/layout.js
+++ src/app/doc/layout.js
@@ -8,6 +8,7 @@ export default function DocsLayout({ children }) {
             <ul className="flex flex-col space-y-1">
               <li><Link href="/doc">Server</Link></li>
               <li><Link href="/doc/IDE">IDE</Link></li>
+              <li><Link href="/doc/github_pages">Github Pages</Link></li>
               <li><Link href="/doc/Server_Mieten">Server Mieten</Link></li>
               <li><Link href="/doc/Analysetool">Analysetool</Link></li>
               <li><Link href="/doc/Webentwicklung">Webentwicklung</Link></li>
