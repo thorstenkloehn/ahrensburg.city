e06c068b11791e8b866c007d8b93b0226e820791
Thorsten Klöhn <thorstenkloehn@gmail.com>
2024-04-22 18:40:56 +0200

    start

--- src/app/Ahrensburg/layout.js
+++ src/app/Ahrensburg/layout.js
@@ -9,6 +9,9 @@ export default function DocsLayout({ children }) {
               <li><Link href="/Ahrensburg">Nachtleben</Link></li>
               <li><Link href="/Ahrensburg/Amt">Amt</Link></li>
               <li><Link href="/Ahrensburg/Bauwerke">Bauwerke</Link></li>
+              <li><Link href="/Ahrensburg/Parks">Parks</Link></li>
+              <li><Link href="/Ahrensburg/Naturschutzgebiet">Naturschutzgebiete</Link></li>
+              
               </ul>
           </div>
           <div className="flex-grow pl-7 w-full">{children}</div>
