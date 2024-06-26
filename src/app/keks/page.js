95edd16c3e378bc0ac62263c8e75443b4540ccbc
Your Name <thorstenkloehn@gmail.com>
2024-06-26 09:24:00 +0200

    statz

---
+++ src/app/keks/page.js
@@ -0,0 +1,13 @@
+import { cookies } from 'next/headers'
+ 
+export default function Page() {
+  const cookieStore = cookies()
+  return cookieStore.getAll().map((cookie) => (
+    <div key={cookie.name}>
+      <p>Name: {cookie.name}</p>
+      <p>Value: {cookie.value}</p>
+
+      <p>Hallo T</p>
+    </div>
+  ))
+}
