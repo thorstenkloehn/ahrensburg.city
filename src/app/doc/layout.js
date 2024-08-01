633359f4949d28b302cf35fa26ff37a94e30e93d
Thorsten Klöhn <thorstenkloehn@gmail.com>
2024-08-01 13:41:46 +0200

    start

--- src/app/doc/layout.js
+++ src/app/doc/layout.js
@@ -9,11 +9,11 @@ export default function DocsLayout({ children }) {
               <li><Link href="/doc">Server</Link></li>
               <li><Link href="/doc/IDE">IDE</Link></li>
               <li><Link href="/doc/github_pages">Github Pages</Link></li>
+              <li><Link href="/doc/Wordpress">Wordpress</Link></li>
               <li><Link href="/doc/Server_Mieten">Server Mieten</Link></li>
               <li><Link href="/doc/Analysetool">Analysetool</Link></li>
               <li><Link href="/doc/Fullstack">Fullstack</Link></li>
               <li><Link href="/doc/Next.js">Next.js</Link></li>
-              <li><Link href="/doc/Strapi">Strapi</Link></li>
               <li><Link href="/doc/Webentwicklung">Webentwicklung</Link> </li>
               <li><Link href="/doc/WebFrameworkBenchmarks">Benchmark</Link></li>
               </ul>
